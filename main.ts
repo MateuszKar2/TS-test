b4eb73dba065592c67c31bcfbcf44d3875e5a760
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-01-01 20:08:39 +0100

    add file main

---
+++ main.ts
@@ -0,0 +1,12 @@
+//Działający kod w JS jest również kodem TS
+
+//TS to dodatkowa powłoka, która pokrywa mój kod DZIĘKI CZEMU widzę więcej i wyraźniej, nieoczekiwanych zachowań w moim kodzie
+
+//JS udostepnia podstawowe type
+//Jednak TS znacznie to rozbudowóje 
+
+//Za pomocą TS mogę tworzyć złożone typy(unie i typy generyczne)
+
+
+
+
