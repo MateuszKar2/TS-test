6382b19b993e94c3c823432ef1df4168945600ca
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-02-11 13:17:51 +0100

    add file with index

--- kurs-TypeScript/src/index.ts
+++ kurs-TypeScript/src/index.ts
@@ -1,9 +1,1 @@
 
-//adnotacje pozwalają nam określić typ danych
-
-const firstName: string = "John";
-
-const numberFlowers: number = 15;
-
-//adnotacje pozwalają już na wczesnym etapie kodu, poinformować że coś już nie gra
-
