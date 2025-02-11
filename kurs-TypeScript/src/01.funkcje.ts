307ac50d06a89c921e77db46ceeda71607449227
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-02-11 20:42:13 +0100

    add file with funkcje

---
+++ kurs-TypeScript/src/01.funkcje.ts
@@ -0,0 +1,37 @@
+
+//function
+const doSomething = (a: number = 5, b:number): number | string => {
+    if (a > 10) {
+        return a / b
+    }
+    return `Przepraszam, ta liczba - ${a + b} jest zbyt mała`
+};
+
+
+console.log(doSomething(2, 12))
+
+
+
+//never-nigdy nie powinniśmy zwracać wartości
+//Funkcja nigdy nie przestaje być wykonywana
+//:void vs :never                 ??????????
+
+const twoFer = (person: string = "you"): string => {
+    return `One for ${person}, one for me`
+}
+
+console.log(twoFer())
+console.log(twoFer("Elvid"));
+
+twoFer()
+
+
+
+
+
+
+const isLeapyear = (year: number): boolean => {
+     return(year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+     };
+console.log(isLeapyear(2012));
+console.log(isLeapyear(2013));
