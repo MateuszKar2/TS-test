ea30d58f282ca896c0661086630a5095da7134b1
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-03-04 08:53:48 +0100

    add file

---
+++ kurs-TypeScript/.praktyka/15.react.ts
@@ -0,0 +1,33 @@
+//1)isnstalujemy TS z Reactem
+//gdy korzystamy z create-react-app - najbardziej popularna opcja
+//create-react-app my-app --temlate typescript
+
+//.tsx jest rozszerzeniem plików w react
+
+
+
+//typescript-cheatsheets/react -sciąga do TS w React
+
+
+
+
+//dwa sposoby pisanie komponentów funkcyjnych
+//starsza wersja ))
+// const Greeter: RecordingState.FC = () => {
+//     return <h1>HELLO!!!!</h1>;
+// };
+
+// export default Greeter;
+
+//nowa wersja
+// function Greeter(): JSX.Element {
+//     return <h1>Hello!</h1>;
+// };
+
+
+
+
+
+
+//propsy
+//najlepszym i najczystszym sposobem są interface
