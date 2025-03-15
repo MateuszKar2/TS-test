8744074fb6bbc041f4582ccb658eb1c8f0f9c12c
Mateusz Karpiński <111981363+MateuszKar2@users.noreply.github.com>
2025-03-15 21:16:23 +0100

    add file

---
+++ kurs-TypeScript/.kurs/02.tsreact.ts
@@ -0,0 +1,27 @@
+
+//children
+
+//używana z children
+// interface CourseGoalProps {
+//     title: string;
+//     children: ReactNode
+// }
+
+//używana children + inne typy
+// type CourseGoalProps = PropsWithChildren<{title: string}>;
+
+
+
+
+
+
+
+//:FC - komponent funkcyjny - funkcje którą przypiszemy takiemu komponentowi, będzie komponentem funkcyjnym
+
+
+
+
+
+
+
+//hooki-np.useState[] potrzebne jest określenie typu danych przy typach złożonych np. tablice lub obiekty
